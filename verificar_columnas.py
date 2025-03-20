b6b8b58295f526efe715e13ac170eed71a7e4e06
robi010 <rbragigl@myuax.com>
2025-03-20 13:02:19 +0100

    Eliminando archivos innecesarios del repositorio

--- verificar_columnas.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# Cargar el archivo Excel desde la fila 3
-df = pd.read_excel("base_datos_partidas.xlsx", header=2)
-
-# Mostrar los nombres de las columnas detectados
-print("Columnas detectadas:", df.columns)
